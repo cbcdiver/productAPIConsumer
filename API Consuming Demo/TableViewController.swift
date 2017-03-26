3fe303b56940e107b79a9ce8f0dd6d08d3a8bddb
Chris Chadillon <chris@chadillon.com>
2017-03-26 15:02:22 -0400

    Table fills

--- API Consuming Demo/TableViewController.swift
+++ API Consuming Demo/TableViewController.swift
@@ -30,35 +30,37 @@ class TableViewController: UITableViewController {
         APIInteractions.getAllData(theURL: APIDetils.buildUrl(theAPICallIndex: APICalls.allProducts.theIndex),
                                    onCompletion: { (theProducts: [Product]?) -> () in
                                     self.theProducts = theProducts!
+                                    
                                     DispatchQueue.main.async(execute: { () -> Void in
-                                        print("Count = \(self.theProducts.count)")
+                                        self.tableView.reloadData()
                                     })
         })
         
-
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return theProducts.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TheCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = self.theProducts[indexPath.row].name
+        cell.detailTextLabel?.text = self.theProducts[indexPath.row].price.description
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
